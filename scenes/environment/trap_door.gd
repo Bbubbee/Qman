2399cff05358caffb8907fb81e97ae20fd724610
Darren Borromeo <darrenborromeodrb@gmail.com>
2024-03-26 18:08:40 +1100

    finished trap room

--- scenes/environment/trap_door.gd
+++ scenes/environment/trap_door.gd
@@ -2,6 +2,15 @@ extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var static_body_2d = $StaticBody2D
+@onready var hurtbox: Hurtbox = $Hurtbox
+@onready var animation_player_2: AnimationPlayer = $AnimationPlayer2
+
+signal destroyed
+
+
+func _ready() -> void:
+	hurtbox.monitoring = false
+	hurtbox.monitorable = false
 
 
 ## Spawn the door to trap the player inside. 
@@ -9,4 +18,23 @@ func spawn_door():
 	static_body_2d.collision_layer = 1
 	animation_player.play("spawn")
 	animation_player.queue("idle")
+	
 
+
+func disable(): 
+	animation_player.play("death")
+	hurtbox.monitoring = true
+	hurtbox.monitorable = true
+
+
+
+func _on_health_component_handle_attack(attack: Hitbox, has_died: bool) -> void:
+	if has_died: 
+		call_deferred("particle_ass_booty", attack)
+		destroyed.emit()
+		queue_free()
+	else: 
+		animation_player_2.play("hurt")
+
+func particle_ass_booty(attack): 
+	Events.spawn_particles.emit(self.global_position, 20, attack.direction)
--- scenes/environment/trap_room.gd
+++ scenes/environment/trap_room.gd
@@ -17,15 +17,28 @@ extends Node2D
 func _ready() -> void:
 	for actor in actors.get_children(): 
 		if actor.has_signal("died"): 
-			actor.died.connect(check_if_room_is_cleared)
+			actor.died.connect(unlock_room)
 		elif actor.has_signal("everyones_dead"):
-			actor.everyones_dead.connect(check_if_room_is_cleared)
-
-
-func check_if_room_is_cleared():
-	print("i died")
+			actor.everyones_dead.connect(unlock_room)
 	
-	if actors.get_child_count()-1 <= 0: 
+	for door in trap_doors.get_children(): 
+		door.destroyed.connect(free_room)
+
+
+## Checks if we should unlock the room. 
+## This happens when all relevant actors are defeated. 
+func unlock_room():	
+	if actors.get_child_count()-1 <= 0: 		
+		# Disable doors. 
+		for door in trap_doors.get_children(): 
+			door.disable()
+
+
+## Once the room is unlocked, the doors can be destroyed. 
+## Once all doors are destroyed, free the room. 
+func free_room(): 
+	if trap_doors.get_child_count()-1 <= 0: 	
+		print("freeing room")	
 		queue_free()
 
 
